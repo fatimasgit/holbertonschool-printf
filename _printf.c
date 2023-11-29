26798cc03863e86800af0e2c326e812de211b314
Ravan Asad <ravanasad@outlook.com>
2023-11-29 03:59:30 -0800

    Added print_int function to _printf.c

--- _printf.c
+++ _printf.c
@@ -2,7 +2,8 @@
 #include <unistd.h>
 #include <stdarg.h>
 #include <stdlib.h>
-
+#include <math.h>
+#include "main.h"
 /**
  * print_char - print char
  * @list: list of args
@@ -10,10 +11,9 @@
  */
 void print_char(va_list list, int *length)
 {
-	int a = va_arg(list, int);
-
-	write(1, &a, 1);
-	(*length)++;
+        int a = va_arg(list, int);
+        write(1, &a, 1);
+        (*length)++;
 }
 /**
  * print_str - print string
@@ -22,49 +22,83 @@ void print_char(va_list list, int *length)
  */
 void print_str(va_list list, int *length)
 {
-	char *str = va_arg(list, char *);
-	int j = 0;
-
-	if (str == NULL)
-	{
-		write(1, "(null)", 6);
-		(*length) += 6;
-		return;
-	}
-
-	for (; str[j]; j++)
-	{
-		write(1, &str[j], 1);
-		(*length)++;
-	}
+        char *str = va_arg(list, char *);
+        int j = 0;
+        if (str == NULL)
+        {
+                write(1, "(null)", 6);
+                (*length) += 6;
+                return;
+        }
+        for (; str[j]; j++)
+        {
+                write(1, &str[j], 1);
+                (*length)++;
+        }
 }
-
 /**
- * checkIsCharOrStr - check is char or string
+ * print_int - print integer
+ * @list: list of args
+ * @length: length of text
+ */
+void print_int(va_list list, int *length)
+{
+        int len_of_int = -1;
+        int a;
+        int temp = va_arg(list, int);
+        int copy_of_temp = temp;
+        
+	if (temp < 0)
+	{
+		temp = copy_of_temp *= -1;
+		write(1, "-", 1);
+	}	
+	while (copy_of_temp != 0)
+        {
+                len_of_int++;
+                copy_of_temp /= 10;
+        }
+        *(length) += len_of_int + 1;
+        while (len_of_int != 0)
+        {
+        a = (temp / (int)(custom_pow(10, len_of_int))) + 48;
+        write(1, &a, 1);
+        temp %= (int)(custom_pow(10, len_of_int));
+        len_of_int--;
+        }
+        a = temp + 48;
+        write(1, &a, 1);
+}
+/**
+ * find_type - check is char or string
  * @list: list of args
  * @format: format for checking
  * @length: total length of str
  */
-void checkIsCharOrStr(va_list list, char format, int *length)
+void find_type(va_list list, char format, int *length)
 {
-	switch (format)
-	{
-	case 'c':
-		print_char(list, length);
-		break;
-	case 's':
-		print_str(list, length);
-		break;
-	case '%':
-		write(1, "%", 1);
-		(*length)++;
-		break;
-	default:
-		write(1, "%", 1);
-		write(1, &format, 1);
-		(*length) += 2;
-		break;
-	}
+        switch (format)
+        {
+        case 'c':
+                print_char(list, length);
+                break;
+        case 's':
+                print_str(list, length);
+                break;
+        case '%':
+                write(1, "%", 1);
+                (*length)++;
+                break;
+        case 'd':
+        case 'i':
+                print_int(list, length);
+                break;
+        default:
+                write(1, "%", 1);
+                write(1, &format, 1);
+                (*length) += 2;
+                break;
+        }
 }
 /**
  * _printf - copy of printf
@@ -74,34 +108,30 @@ void checkIsCharOrStr(va_list list, char format, int *length)
 */
 int _printf(const char *format, ...)
 {
-	int length = 0;
-	va_list list;
-	int i = 0;
-
-	if (format == NULL)
-	{
-		exit(98);
-	}
-
-	va_start(list, format);
-
-	for (; format[i] != '\0'; i++)
-	{
-		if (format[i] == '%' && format[i + 1] == '\0')
-		{
-			return (-1);
-		}
-		else if (format[i] == '%')
-		{
-			checkIsCharOrStr(list, format[++i], &length);
-		}
-		else
-		{
-			write(1, &format[i], 1);
-			length++;
-		}
-	}
-
-	va_end(list);
-	return (length);
+        int length = 0;
+        va_list list;
+        int i = 0;
+        if (format == NULL)
+        {
+                exit(98);
+        }
+        va_start(list, format);
+        for (; format[i] != '\0'; i++)
+        {
+                if (format[i] == '%' && format[i + 1] == '\0')
+                {
+                        return (-1);
+                }
+                else if (format[i] == '%')
+                {
+                        find_type(list, format[++i], &length);
+                }
+                else
+                {
+                        write(1, &format[i], 1);
+                        length++;
+                }
+        }
+        va_end(list);
+        return (length);
 }
---
+++ custom_pow.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * custom_pow - find power of int
+ * @base: is always 10
+ * @power: power
+ * Return: the base of the power
+ */
+int custom_pow(int base, int power)
+{
+        if (power == 0)
+                return (1);
+        return (base * custom_pow(base, power - 1));
+}
